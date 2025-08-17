14fd770c1460b28ac4e0ee32d273e4142119a9e8
CarvalhoCortes <carvalhocortes@gmail.com>
2025-08-17 14:59:25 -0300

    refactor: reorganize ESLint configuration and remove unused parser options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,24 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
 import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**', '*.js'],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       globals: globals.node,
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-      },
     },
     plugins: {
-      '@typescript-eslint': tseslint,
       prettier: prettierPlugin,
     },
     rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
       'prettier/prettier': 'error',
       'max-len': ['error', { code: 130 }],
       '@typescript-eslint/no-explicit-any': 'off',
