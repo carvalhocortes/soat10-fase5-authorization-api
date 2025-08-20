2f785143ad2af8f296ccb550219452b1ec65f41f
CarvalhoCortes <carvalhocortes@gmail.com>
2025-08-20 09:30:35 -0300

    fix: update lambda function paths to use relative directory structure

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,8 +6,8 @@ resource "aws_ssm_parameter" "jwt_secret" {
 
 resource "aws_lambda_function" "auth" {
   function_name    = "auth"
-  filename         = "./lambda.zip"
-  source_code_hash = filebase64sha256("./lambda.zip")
+  filename         = "../lambda.zip"
+  source_code_hash = filebase64sha256("../lambda.zip")
   handler          = "index.authHandler"
   runtime          = "nodejs22.x"
   role             = "arn:aws:iam::${var.aws_account_id}:role/LabRole"
@@ -23,8 +23,8 @@ resource "aws_lambda_function" "auth" {
 
 resource "aws_lambda_function" "create_user" {
   function_name    = "create-user"
-  filename         = "./lambda.zip"
-  source_code_hash = filebase64sha256("./lambda.zip")
+  filename         = "../lambda.zip"
+  source_code_hash = filebase64sha256("../lambda.zip")
   handler          = "index.createUserHandler"
   runtime          = "nodejs22.x"
   role             = "arn:aws:iam::${var.aws_account_id}:role/LabRole"
