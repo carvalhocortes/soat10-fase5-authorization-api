1ba50ec255fedb50e48042a8830b4c07b3436f1e
CarvalhoCortes <fernando.minero@gmail.com>
2025-09-29 21:59:22 -0300

    feat: configure IAM roles for Lambda functions and update role refere… (#5)
    * feat: configure IAM roles for Lambda functions and update role references in Lambda resources
    
    * refactor: standardize naming conventions in workflow files and remove unused AWS_ACCOUNT_ID variable
    
    ---------
    
    Co-authored-by: CarvalhoCortes <carvalhocortes@gmail.com>

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postech-soat10-bucket-fase5"
+    bucket = "fiap-terraform-backend"
     key    = "github-actions-fiap/auth-api/terraform.tfstate"
 
     region  = "us-west-2"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,8 +3,3 @@ variable "AWS_REGION" {
   default = "us-west-2"
 }
 
-variable "AWS_ACCOUNT_ID" {
-  type    = string
-  default = "548226336065"
-}
-
